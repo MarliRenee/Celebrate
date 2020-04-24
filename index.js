9c3626966edecfa7d8b5063f4ebe0c9330c1a658
Marli Renee <marlirenee@gmail.com>
2020-04-24 11:25:10 -0700

    first draft one country plus year

---
+++ index.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const apiKey = '6a93fc0ceb56dd88e4f84f7a75315a5ec21c7a05'; 
+const searchURL = 'https://calendarific.com/api/v2/holidays';
+
+
+function formatQueryParams(params) {
+  const queryItems = Object.keys(params)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+  return queryItems.join('&');
+}
+
+function displayResults(responseJson) {
+  console.log(responseJson.response.holidays);
+  const hol = responseJson.response.holidays;
+  $('#results-list').empty();
+  for (let i = 0; i < hol.length; i++){
+    $('#results-list').append(
+      `<li><h3>${hol[i].name}</h3>
+      <p>${hol[i].description}</p>
+      </li>`
+    )};
+  $('#results').removeClass('hidden');
+};
+
+function getHolidays(country, year) {
+  const params = {
+    api_key: apiKey,
+    country: country,
+    year: year,
+  };
+  const queryString = formatQueryParams(params)
+  const url = searchURL + '?' + queryString;
+
+  fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(responseJson => displayResults (responseJson))
+    .catch(err => {
+      $('#js-error-message').text(`Something went wrong: ${err.message}`);
+    });
+}
+
+function watchForm() {
+  $('form').submit(event => {
+    event.preventDefault();
+    const year = $('#year').val();
+    const country = $('#country').val();
+    getHolidays(country, year);
+  });
+}
+
+$(watchForm);
